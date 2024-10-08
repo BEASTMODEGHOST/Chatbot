c7cbac44973fc458b3be1a551f8d970406c5504d
BEASTMODEGHOST <106429953+BEASTMODEGHOST@users.noreply.github.com>
2024-10-08 19:52:08 +0530

    Add files via upload

---
+++ Chatbot.py
@@ -0,0 +1,68 @@
+import google.generativeai as genai
+from PIL import Image
+import os
+import PyPDF2
+
+
+GOOGLE_API_KEY = 'AIzaSyD28J8FyR2NI65oGvI9DHJmjlWUxEpDZUI'
+genai.configure(api_key=GOOGLE_API_KEY)
+
+def process_images(image_dir='image_data'):
+    images = []
+    for file in os.listdir(image_dir):
+        if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
+            img_path = os.path.join(image_dir, file)
+            images.append(Image.open(img_path))
+    
+    if not images:
+        raise FileNotFoundError(f"No image files found in the {image_dir} directory.")
+    return images
+
+def process_file(file_path):
+    _, extension = os.path.splitext(file_path)
+    if extension.lower() == '.pdf':
+        return process_pdf(file_path)
+    else:
+        return process_text(file_path)
+
+def process_text(file_path):
+    with open(file_path, 'r') as file:
+        return file.read()
+
+def process_pdf(file_path):
+    with open(file_path, 'rb') as file:
+        pdf_reader = PyPDF2.PdfReader(file)
+        text = ''
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        return text
+
+
+
+def main():
+    file_type = input("Enter file type (image/file): ").lower()
+
+    if file_type == "image":
+        content = process_images()
+    elif file_type == "file":
+        file_data_dir = 'file_data'
+        files = os.listdir(file_data_dir)
+        if not files:
+            raise FileNotFoundError(f"No files found in the {file_data_dir} directory.")
+        file_path = os.path.join(file_data_dir, files[0])
+        content = process_file(file_path)
+    else:
+        raise ValueError("Invalid file type. Please choose 'image' or 'file'.")
+
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    user_prompt = input("Enter your Query: ")
+    
+    if file_type == "image":
+        response = model.generate_content([user_prompt] + content)
+    else:
+        response = model.generate_content([user_prompt, content])
+    
+    print(response.text)
+
+if __name__ == "__main__":
+    main()
